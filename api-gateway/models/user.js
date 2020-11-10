7b0e6757df5b53094f1dc50259ef9d9d6f4d0cf3
Jonathan Roland <jdroland31@yahoo.com>
2020-11-09 21:50:05 -0500

    added assignment 4.3 requirements

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -13,3 +13,12 @@ var userSchema = new mongoose.Schema({
     email: String
 });
 module.exports = mongoose.model('User', userSchema);
+
+//user.save is used to add a new user in our database
+module.exports.add = (user,callback) => {
+    user.save(callback);
+};
+module.exports.getById = (id,callback) => {
+    var query = {_id: id};
+    User.findById(query,callback);
+};
