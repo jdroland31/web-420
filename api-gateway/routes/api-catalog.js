6f677fb796228412287867aa04d7f2cf62b8d776
Jonathan Roland <jdroland31@yahoo.com>
2020-12-06 21:49:32 -0500

    added files for assingment 8.4

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -8,6 +8,7 @@ Description: api catalog;
 
 //API Routes
 var express = require('express');
+var checkToken = require('../check-token');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
 
@@ -16,7 +17,8 @@ router.post('/auth/register',auth_controller.user_register);
 router.post('/auth/login',auth_controller.user_login);
 
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+// router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 router.get('/auth/logout', auth_controller.user_logout);
 
 module.exports = router;
